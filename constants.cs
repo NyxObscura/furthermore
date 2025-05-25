7124929f43b7000a720a9cf294f0b5c093accd99
Obscura Student <service@obscuraworks.com>
2025-05-25 18:55:39 +0700

    Create constants.cs

---
+++ constants.cs
@@ -0,0 +1,14 @@
+public static class Constants
+{
+    public const double PlanckConstant = 6.62607015e-34; 
+    public const double ReducedPlanckConstant = PlanckConstant / (2 * Math.PI); 
+    public const double ElectronMass = 9.1093837015e-31; 
+    public const double ElementaryCharge = 1.602176634e-19; 
+    public const double SpeedOfLight = 299792458; 
+    public const double BoltzmannConstant = 1.380649e-23; 
+    public const double VacuumPermittivity = 8.8541878128e-12; 
+    public const double VacuumPermeability = 1.25663706212e-6; 
+
+    
+    public const double JoulesPerElectronVolt = 1.602176634e-19;
+}
